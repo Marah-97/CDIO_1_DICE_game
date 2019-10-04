bcb2515d2a815c1757612a21be3c622922e2853f
Marah-97 <55982299+Marah-97@users.noreply.github.com>
2019-10-04 12:55:46 +0200

    Update Main.java
    print out a message at line 10

--- Dice game/src/com/company/Main.java
+++ Dice game/src/com/company/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        
+        System.out.println("The player who get 40 points first wins.");
+            
 //Enter players name for a more interactiv game
         System.out.println("what is player 1´s name?");
         String name1 = input.nextLine();
